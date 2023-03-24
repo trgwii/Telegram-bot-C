8bd20d8bceb0b56fe6b4dd3f2e12a5ddc106f3f7
Thomas Rory Gummerson <thomas@gummerson.no>
2023-03-24 15:53:04 +0100

    Updated build system

--- build.c
+++ build.c
@@ -1,47 +1,72 @@
-// zig run -lc build.c
-
+#include "build.h"
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <string.h>
 
-#define CC "zig cc"
-#define LIBS "-lcurl -lsqlite3"
+int main(int argc, char **argv) {
+  bool clean = false;
+  bool self = false;
+  bool fetch = false;
+  bool test = false;
+  bool build = false;
 
-#define DISABLED                                                               \
-  "-Wno-disabled-macro-expansion "                                             \
-  "-Wno-padded "                                                               \
-  "-Wno-declaration-after-statement"
-#define FLAGS                                                                  \
-  "-Wall "                                                                     \
-  "-Wextra "                                                                   \
-  "-Wpedantic "                                                                \
-  "-Weverything "                                                              \
-  "-Werror " DISABLED
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "clean") == 0)
+      clean = true;
+    else if (strcmp(argv[i], "self") == 0)
+      self = true;
+    else if (strcmp(argv[i], "fetch") == 0)
+      fetch = true;
+    else if (strcmp(argv[i], "test") == 0)
+      test = true;
+    else if (strcmp(argv[i], "build") == 0)
+      build = true;
+  }
 
-#define HTTP_GET(url, file) system("curl -s " url " -o " file)
-#define OBJ(name) system(CC " " FLAGS " src/" name ".c -c -o o/" name ".o")
-#define EXE(cmd) system(CC " " FLAGS " " LIBS " " cmd)
-#define DIR(name) mkdir(name, 0755)
-#define TEST(name)                                                             \
-  system(CC " " FLAGS " src/" name "_test.c -o test/" name " && ./test/" name  \
-            " > /dev/null")
+  if (!clean && !self && !fetch && !test && !build) {
+    printf("Usage:\n\tzig run -lc build.c -- [clean] [self] [fetch] [test] "
+           "[build]\n");
+    return 1;
+  }
 
-int main(void) {
-  // Deps
-  HTTP_GET("https://raw.githubusercontent.com/sheredom/json.h/master/json.h",
-           "src/json.h");
+  if (clean) {
+    double start = measure_start();
+    RM("test");
+    RM("o");
+    RM("build");
+    measure_end("clean", start);
+  }
 
-  // Tests
-  DIR("test");
-  TEST("str");
+  if (self) {
+    double start = measure_start();
+    CMD(CC " " FLAGS " build.c -o - > /dev/null");
+    measure_end("self", start);
+  }
 
-  // Object files
-  DIR("o");
-  OBJ("str");
-  OBJ("bot");
+  if (fetch) {
+    double start = measure_start();
+    HTTP_GET("https://raw.githubusercontent.com/sheredom/json.h/master/json.h",
+             "src/json.h");
 
-  // Build
-  DIR("build");
-  EXE("o/*.o src/main.c -o build/main");
+    measure_end("fetch", start);
+  }
 
-  return 0;
+  if (test) {
+    double start = measure_start();
+    DIR("test");
+    TEST("str");
+    measure_end("test", start);
+  }
+
+  if (build) {
+    double start = measure_start();
+    DIR("o");
+    OBJ("str");
+    OBJ("bot");
+
+    DIR("build");
+    EXE("o/*.o src/main.c -o build/main");
+    measure_end("build", start);
+  }
+
+  return done();
 }
---
+++ build.h
@@ -0,0 +1,92 @@
+// zig run -lc build.c -- self fetch test build
+#define _GNU_SOURCE
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#define CC "zig cc"
+#define LIBS "-lcurl -lsqlite3"
+
+#define DISABLED                                                               \
+  "-Wno-disabled-macro-expansion "                                             \
+  "-Wno-padded "                                                               \
+  "-Wno-declaration-after-statement"
+#define FLAGS                                                                  \
+  "-Wall "                                                                     \
+  "-Wextra "                                                                   \
+  "-Wpedantic "                                                                \
+  "-Weverything "                                                              \
+  "-Werror " DISABLED
+
+static bool ok = true;
+static char *failed = NULL;
+static bool measured = false;
+static char measured_col = 0;
+static char measured_col_max = 60;
+static bool measured_failing = false;
+static double measured_total = 0;
+
+#define DIR(name) mkdir(name, 0755)
+
+#define CMD(command)                                                           \
+  do {                                                                         \
+    char *cmd = command;                                                       \
+    if (ok && system(cmd) != 0) {                                              \
+      ok = false;                                                              \
+      failed = cmd;                                                            \
+    }                                                                          \
+  } while (false)
+
+#define HTTP_GET(url, file) CMD("curl -s " url " -o " file)
+
+#define OBJ(name) CMD(CC " " FLAGS " src/" name ".c -c -o o/" name ".o")
+
+#define EXE(cmd) CMD(CC " " FLAGS " " LIBS " " cmd)
+
+#define TEST(name)                                                             \
+  CMD(CC " " FLAGS " src/" name "_test.c -o test/" name " && ./test/" name     \
+         " > /dev/null")
+
+#define RM(name) system("rm -r " name " 2> /dev/null")
+
+static inline double measure_start(void) {
+  struct timespec start;
+  clock_gettime(CLOCK_REALTIME, &start);
+  return (double)start.tv_sec + (double)start.tv_nsec / 1000000000;
+}
+
+static inline void measure_end(char *name, double start) {
+  struct timespec end;
+  clock_gettime(CLOCK_REALTIME, &end);
+  if (measured_failing)
+    return;
+  double measured_time =
+      (((double)end.tv_sec + (double)end.tv_nsec / 1000000000) - start) * 1000;
+  printf("%s%s:\t\x1b[33m%f\x1b[0mms",
+         measured ? measured_col >= measured_col_max ? "\n" : "\t" : "", name,
+         measured_time);
+  if (measured_col >= measured_col_max)
+    measured_col = 0;
+  measured = true;
+  measured_failing = !ok;
+  measured_total += measured_time;
+  measured_col += 20;
+}
+
+static inline int done(void) {
+  printf("%stotal: \x1b[33m%f\x1b[0mms",
+         measured_col >= measured_col_max ? "\n" : "\t", measured_total);
+  if (measured_col >= measured_col_max)
+    measured_col = 0;
+  measured_col += 20;
+  printf("%s%s%s\n", measured_col >= measured_col_max ? "\n" : "\t",
+         ok ? "\x1b[32m== OK ==\x1b[0m" : "\x1b[31m!! FAILED !!\x1b[0m",
+         failed ? ": " : "");
+  if (failed) {
+    printf("\x1b[31m%s\x1b[0m\n", failed);
+  }
+
+  return ok == true;
+}
