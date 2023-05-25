4adf8f8785dee609fc1ede0b555e4899e62a4b73
Thomas Rory Gummerson <thomas@gummerson.no>
2023-05-25 12:27:28 +0200

    Bugfix cli_command

--- build.h
+++ build.h
@@ -99,7 +99,7 @@ static void measure_end(char *name, double start) {
 
 // Check if a CLI command was passed (or always true if "all" was passed)
 static bool cli_command(int argc, char **argv, char *command) {
-  for (int i = 0; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "all") == 0 || strcmp(argv[i], command) == 0)
       return true;
   }
